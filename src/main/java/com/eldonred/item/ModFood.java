71e338e5196f58a7f5d8e01227f10cf4d9cc5c86
Eldon-Red <stanleyereed7@gmail.com>
2025-08-04 14:01:39 -0500

    First Item: Almond Water

---
+++ src/main/java/com/eldonred/item/ModFood.java
@@ -0,0 +1,7 @@
+package com.eldonred.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFood {
+        public static final FoodComponent ALMOND_WATER = new FoodComponent.Builder().hunger(4).saturationModifier(.25f).alwaysEdible().build();
+}
--- src/main/java/com/eldonred/item/ModItems.java
+++ src/main/java/com/eldonred/item/ModItems.java
@@ -1,4 +1,31 @@
 package com.eldonred.item;
 
+import com.eldonred.Liminality;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+
+
 public class ModItems {
+
+    public static final Item ALMOND_WATER = registerItem("almond_water",
+            new HoneyBottleItem(new Item.Settings().food(ModFood.ALMOND_WATER)));
+
+
+
+
+
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Liminality.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems() {
+    }
 }
