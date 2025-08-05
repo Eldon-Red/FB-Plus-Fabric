b2144f066cfbdfb01cdac06e49e5f89b92c21642
Eldon-Red <stanleyereed7@gmail.com>
2025-08-04 21:18:58 -0500

    Some blocks

--- src/main/java/com/eldonred/Liminality.java
+++ src/main/java/com/eldonred/Liminality.java
@@ -1,5 +1,6 @@
 package com.eldonred;
 
+import com.eldonred.block.ModBlocks;
 import com.eldonred.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class Liminality implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/com/eldonred/block/ModBlocks.java
@@ -0,0 +1,36 @@
+package com.eldonred.block;
+
+import com.eldonred.Liminality;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static final Block YELLOW_WALLPAPER = registerBlock("yellow_wallpaper",
+            new Block(FabricBlockSettings.copyOf(Blocks.BEDROCK).sounds(BlockSoundGroup.WOOD)));
+    public static final Block DAMP_CARPET = registerBlock("damp_carpet",
+            new Block(FabricBlockSettings.copyOf(Blocks.BEDROCK).sounds(BlockSoundGroup.WOOL)));
+
+
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(Liminality.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(Liminality.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+
+}
+
+    public static void registerModBlocks() {
+    }
+}
